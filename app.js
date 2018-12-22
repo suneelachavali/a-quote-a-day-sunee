521bfb2f434ae03dcc70f2150f2feae854761e28
suneelachavali <46063928+suneelachavali@users.noreply.github.com>
2018-12-22 02:40:01 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(request, response){
-    response.out('Hello Heroku!!');
+    response.end('Hello Heroku!!');
     /* response.sendFile('https://github.com/suneelachavali/a-quote-a-day-sunee/blob/master/index.html'); */
 });
 
